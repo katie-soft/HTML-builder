0a28c9c1c11b9b0f0fd4784477ca92dbcf7e5e9c
katie-soft <analyst.meo@gmail.com>
2023-05-03 12:13:12 +0300

    feat: solve task 4 Copy directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,47 @@
+const { copyFile } = require('fs');
 
+fs = require('fs');
+path = require('path');
+
+function copyDir(source, target) {
+  function handleError(err) {
+    if (err) throw err;
+  }
+
+  function createFiles() {
+    fs.readdir(path.join(__dirname, source), (err, files) => {
+      if (err) {
+        console.log(err);
+      } else {
+        files.forEach(file => {
+          copyFile(path.join(__dirname, source, file), path.join(__dirname, target, file), handleError);
+        })
+      }
+    });
+  }
+
+  function deleteFiles(folder) {
+    fs.readdir(path.join(__dirname, folder), (err, files) => {
+      if (err) {
+        console.log(err);
+      } else {
+        files.forEach(file => {
+          fs.unlink(path.join(__dirname, folder, file), handleError);
+        })
+      }
+    });
+  }
+
+  fs.stat(path.join(__dirname, target), (err) => {
+    if (!err) {
+      deleteFiles(target);
+      createFiles();
+    }
+    else if (err.code === 'ENOENT') {
+      fs.mkdir(path.join(__dirname, target), handleError);
+      createFiles();
+    }
+  })
+}
+
+copyDir('files', 'copy-files');
