aa7c4ae04f83d3a95059e8e4807fd11e077a79bc
katie-soft <analyst.meo@gmail.com>
2023-05-08 11:45:32 +0300

    fix: fix algorithm for copying assets in task 6

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -2,74 +2,89 @@ fs = require('fs');
 path = require('path');
 
 //create folders & streams
-fs.mkdir(path.join(__dirname, 'project-dist', 'assets'), { recursive: true }, () => {});
-console.log('step 1')
+fs.mkdir(path.join(__dirname, 'project-dist', 'assets'), { recursive: true }, () => {
+  console.log('start building page');
+  createStreams();
+  createCSS();
+  createHTML();
+  copyDir(path.join(__dirname, 'assets'), path.join(__dirname, 'project-dist', 'assets'), err => console.log('3', err));
+});
 
-const inputHTML = fs.createReadStream(path.join(__dirname, 'template.html'), 'utf-8');
-const outputHTML = fs.createWriteStream(path.join(__dirname, 'project-dist', 'index.html'));
-const outputCSS = fs.createWriteStream(path.join(__dirname, 'project-dist', 'style.css'));
+let inputHTML,outputHTML, outputCSS = {};
 
-console.log('step 4')
+function createStreams() {
+  console.log('step streams start')
+  inputHTML = fs.createReadStream(path.join(__dirname, 'template.html'), 'utf-8');
+  outputHTML = fs.createWriteStream(path.join(__dirname, 'project-dist', 'index.html'));
+  outputCSS = fs.createWriteStream(path.join(__dirname, 'project-dist', 'style.css'));
+  console.log('step streams finished')
+}
 
 //copy assets
+function createDir(source, target) {
+  fs.readdir(path.join(source), ((error, folders) => {
+    if (error) {
+      console.log('dir - ', error);
+    } else {
+      folders.forEach(folder => fs.mkdir(path.join(target, folder), () => {
+        console.log('folder created');
+        fs.readdir(path.join(source, folder), ((error, files) => {
+          if (error) {
+            console.log('file - ', error)
+          } else {
+            files.forEach(file => fs.copyFile(path.join(source, folder, file), path.join(target, folder, file), (error) => console.log('file created')))
+          }
+        }))
+      }))      
+    }
+  }))
+}
+
+function deleteFiles(folder) {
+  fs.readdir(path.join(folder), (err, files) => {
+    if (err) {
+      console.log('delete -', err);
+    } else {
+      files.forEach(file => {
+        fs.unlink(path.join(folder, file), (error) => console.log(error));
+      })
+    }
+  });
+}
+
 function copyDir(source, target) {
-  function handleError(err) {
-    if (err) throw err;
-  }
-
-  function createFiles() {
-    fs.readdir(path.join(source), (err, files) => {
-      if (err) {
-        console.log('1', err);
-      } else {
-        files.forEach(file => {
-        fs.copyFile(path.join(source, file), path.join(target, file), handleError);
-        })
-      }
-    });
-  }
-
-  function deleteFiles(folder) {
-    fs.readdir(path.join(folder), (err, files) => {
-      if (err) {
-        console.log('2', err);
-      } else {
-        files.forEach(file => {
-          fs.unlink(path.join(folder, file), handleError);
-        })
-      }
-    });
-  }
-
   fs.stat(path.join(target), () => {
+      createDir(source, target);
       //deleteFiles(target);
-      createFiles();
   })
 }
 
-//copyDir(path.join(__dirname, 'assets', 'img'), path.join(__dirname, 'project-dist', 'assets', 'img'), err => console.log('3', err));
-
 //build styles
-fs.readdir(path.join(__dirname, 'styles'), (error, files) => {
-  if (error) {
-    console.log('styles - ', error);
-  } else {
-    files.forEach(file => {
-      fs.stat(path.join(__dirname, 'styles', file), (error, stats) => {
-        if (error) {
-          console.log('forEach - ', error);
-        }
-        if (stats.isFile() && path.parse(file).ext === '.css') {
-          const stream = fs.createReadStream(path.join(__dirname, 'styles', file), 'utf-8');
-          stream.on('data', chunk => outputCSS.write(chunk));
-          stream.on('error', error => console.log(error.message));
-        };
+function createCSS() {
+  console.log('step CSS start')
+  fs.readdir(path.join(__dirname, 'styles'), (error, files) => {
+    if (error) {
+      console.log('styles - ', error);
+    } else {
+      files.forEach(file => {
+        fs.stat(path.join(__dirname, 'styles', file), (error, stats) => {
+          if (error) {
+            console.log('styles - ', error);
+          }
+          if (stats.isFile() && path.parse(file).ext === '.css') {
+            const stream = fs.createReadStream(path.join(__dirname, 'styles', file), 'utf-8');
+            stream.on('data', chunk => outputCSS.write(chunk));
+            stream.on('error', error => console.log(error.message));
+          };
+        });
       });
-    });
-  }
-});
+    }
+  });
+  console.log('step CSS finished')
+}
 
 //replace tags in HTML
+const regex = /({{[a-z]+}})/g;
 function readHTMLFile(path) {
   return new Promise((resolve, reject) => {
     let stream = fs.createReadStream(path, 'utf-8');
@@ -107,18 +122,20 @@ function replaceTags(str, obj) {
   return str;
 }
 
-const regex = /({{[a-z]+}})/g;
-let data = '';
-inputHTML.on('data', chunk => data += chunk);
+function createHTML() {
+  console.log('step HTML start')
+  let data = '';
+  inputHTML.on('data', chunk => data += chunk);
 
-inputHTML.on('end', () => {
-  let tags = data.match(regex);
+  inputHTML.on('end', () => {
+    let tags = data.match(regex);
   
   addToObj(tags).then((tagObj) => { 
     let newData = replaceTags(data, tagObj);
     outputHTML.write(newData);
+    });
   });
-});
+  console.log('step HTML finish')
+  inputHTML.on('error', error => console.log('read error - ', error));
 
-inputHTML.on('error', error => console.log('read error - ', error));
-//outputHTML.on('error', error => console.log('write error - ', error));
+}
